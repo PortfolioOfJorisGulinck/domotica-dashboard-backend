4dbbc833e1811cd3cb725eacf07e98a9dffdf57a
JorisOpKdg <35931513+JorisOpKdg@users.noreply.github.com>
2021-07-18 14:50:23 +0200

    Adding User and repository JPA infrastructure

--- src/main/java/be/jorisgulinck/domoticaspringbackend/DomoticaSpringBackendApplication.java
+++ src/main/java/be/jorisgulinck/domoticaspringbackend/DomoticaSpringBackendApplication.java
@@ -2,8 +2,10 @@ package be.jorisgulinck.domoticaspringbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class DomoticaSpringBackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/be/jorisgulinck/domoticaspringbackend/UserRepository.java
@@ -0,0 +1,10 @@
+package be.jorisgulinck.domoticaspringbackend;
+
+import be.jorisgulinck.domoticaspringbackend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
