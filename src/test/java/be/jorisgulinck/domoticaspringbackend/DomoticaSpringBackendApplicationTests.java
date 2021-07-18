ab6da6288491848a1b6334eeba50121c80c9bcea
JorisOpKdg <35931513+JorisOpKdg@users.noreply.github.com>
2021-07-18 13:57:47 +0200

    Creating project with spring initializr

---
+++ src/test/java/be/jorisgulinck/domoticaspringbackend/DomoticaSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jorisgulinck.domoticaspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomoticaSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
